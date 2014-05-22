96df393d54e3f63282c17ecf9650d261d3f935c8
hunter b rose <hunter@katiebushdesign.com>
2014-05-22 12:38:56 -0400

    refine build process.

--- functions.php
+++ functions.php
@@ -2,35 +2,54 @@
 
 define("THEME_DIR", get_template_directory_uri());
 
+/*--------------------------------------------------------*\
+	Post Thumbnails
+\*--------------------------------------------------------*/
+
 if ( function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails' );
 	set_post_thumbnail_size();
 }
 
-function jfcs_register_menus() {
+/*--------------------------------------------------------*\
+	Register Navigation Menu
+\*--------------------------------------------------------*/
+
+function thm_register_menus() {
 	register_nav_menus(
 		array(
 			'u-nav' => __( 'Universal Navigation'),
 		)
 	);
 }
-add_action( 'init', 'jfcs_register_menus' );
+add_action( 'init', 'thm_register_menus' );
+
+/*--------------------------------------------------------*\
+	Clean Up
+\*--------------------------------------------------------*/
+
+function removeHeadLinks() {
+	remove_action('wp_head', 'rsd_link');
+	remove_action('wp_head', 'wlwmanifest_link');
+}
+add_action('init', 'removeHeadLinks');
+remove_action('wp_head', 'wp_generator');
 
 /*--------------------------------------------------------*\
 	Styles and Scripts
 \*--------------------------------------------------------*/
 
-function thm_enqueue_styles() {
-    wp_register_style( 'screen', THEME_DIR . '/bld/screen.min.css', array(), '1', 'all' );
+function thml_enqueue_styles() {
+    wp_register_style( 'screen', THEME_DIR . '/bld/screen.min.css', array(), '1', 'all');
     wp_enqueue_style( 'screen' );
 }
-add_action( 'wp_enqueue_scripts', 'thm_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'thml_enqueue_styles' );
 
-function thm_enqueue_scripts() {
-	wp_enqueue_script( 'main', THEME_DIR . '/bld/bld.min.js', array('jquery'), true);
+function thml_enqueue_scripts() {
+	wp_enqueue_script( 'main', THEME_DIR . '/bld/bld.min.js', array('jquery'), '1', true);
 }
 
-add_action('wp_enqueue_scripts', 'thm_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'thml_enqueue_scripts');
 
 
 ?>
--- header.php
+++ header.php
@@ -1,38 +1,31 @@
 <!doctype html>
 <html class="no-js">
-<!--[if IE]><div style='clear: both; height: 112px; padding:0; position: relative;'><a href="http://www.theie9countdown.com/ie-users-info"><img src="http://www.theie9countdown.com/assets/badge_iecountdown.png" border="0" height="112" width="348" alt="" /></a></div><![endif]-->
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
-	<link rel="stylesheet" type="text/css" href="//cloud.typography.com/7655652/762784/css/fonts.css" />
 	<?php wp_head(); ?>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/lib/modernizr.min.js"></script>
 </head>
 
 <body>
 	<div class="wrapper">
 		<header class="header">
 			<div class="ctn">
-				<div class="header-top">
-					<span data-picture data-alt="Virtual Instruments" class="logo">
-						<span data-src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.min.png"></span>
-					</span>
-					<nav class="u-nav">
-						<?php 
-							$args = array ( 'menu' => 'Universal Navigation', 'container' => false );
-							wp_nav_menu( $args );
-						?>
-					</nav>
-				</div>
-				<div class="header-btm">
-					<nav class="nav">
-						<?php 
-							$args = array ( 'menu' => 'Navigation', 'container' => false );
-							wp_nav_menu( $args );
-						?>				
-					</nav>
-				</div>
+				<div class="logo"><a href="/">
+					<img src="" alt="">
+				</a></div>
+				<nav class="nav">
+					<?php 
+						$args = array ( 'menu' => 'Navigation', 'container' => false );
+						wp_nav_menu( $args );
+					?>
+				</nav>
+<!-- 				<div class="sidr-btn-ctn">
+					<a href="#" id="press" class="navicon-btn x">
+						<div class="navicon"></div>
+					</a>
+				</div>	 -->
 			</div>
-		</header>
-		<div class="header-fill"></div>
+		</header>	
