80dd9f07acb332fb6f32b4f2ea75ba13bbf23cdf
hunter b rose <hunter@katiebushdesign.com>
2014-07-02 08:16:32 -0400

    switch back to susyone.

--- assets/gulp/tasks/styl.js
+++ assets/gulp/tasks/styl.js
@@ -1,19 +1,26 @@
 var gulp         = require('gulp');
-var sass 		 = require('gulp-ruby-sass');
+var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minifycss 	 = require('gulp-minify-css');
 var notify       = require('gulp-notify');
 var rename 		 = require('gulp-rename');
 var handleErrors = require('../util/handleErrors');
+var browserSync  = require('browser-sync');
 
 gulp.task('styl', function() {
     return gulp.src('styl/src/screen.scss')
-        .pipe(sass({sourcemap: false, style: 'compact'}))
+        .pipe(sass({
+            includePaths: [
+                './styl/lib/__vendors/harp-susy/scss',
+                './styl/lib/__vendors/harp-compass/scss'
+            ]
+        }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('styl/bld'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(gulp.dest('../bld'))
         .pipe(notify({ message: 'Styles task complete' }))
+        .pipe(browserSync.reload({ stream: true, notify: false }))
         .on('error', handleErrors);
 });
