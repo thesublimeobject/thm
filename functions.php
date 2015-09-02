13ef7aec594fa48221187251e17bbd095d078221
hunter b rose <hunter@katiebushdesign.com>
2015-09-02 15:21:23 -0400

    Add ACF defaults and Global Options page.

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 define("THEME_DIR", get_template_directory_uri());
 
 require_once 'functions/theme_options.php';
+require_once 'functions/acf.php';
 require_once 'functions/menus.php';
 require_once 'functions/custom.php';
 // require_once 'functions/posts.php';
