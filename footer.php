0aac65f315d127e14094ea875e4f971bd3c844e8
hunter b rose <hunter@katiebushdesign.com>
2016-02-02 09:18:02 -0500

    add new script to footer for local dev.

--- footer.php
+++ footer.php
@@ -2,7 +2,11 @@
 
 			</footer>
 		</div>
-		<script src="/bld/load.js"></script>
-		<?php wp_footer(); ?>
+		<?php if ( strpos($_SERVER['HTTP_HOST'], 'com') > -1 || strpos($_SERVER['HTTP_HOST'], 'io') > -1 ) { 
+			echo '<script src="/bld/app.min.js?' . time() . '"></script>';
+		} else {
+			echo '<script src="/public/bundle.js"></script>';
+		}
+		wp_footer(); ?>
 	</body>
 </html>
