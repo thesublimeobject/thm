e9c03348ef2eb91c53d1cc72ae6343751648316e
bryanlilly <bryan@katiebushdesign.com>
2016-08-08 14:09:49 -0400

    Small banner tweaks

--- inc/loop-banner.php
+++ inc/loop-banner.php
@@ -1,59 +1,71 @@
 <?php 
 	$banners = get_sub_field( 'banner_item' );
+	$length = count( $banners );
+	$html = '';
 ?>
-<section class="block block__banner">
+<section class="block block--banner <?php if ($length > 1) echo 'block--banner-slider'; ?>">
 	<ul class="banner__slider" id="banner__slider">
 	<?php  
-		$html = '';
+		
+		if ($length > 1) $html .= '<ul class="banner__slider" id="banner__slider">';
+		
 		foreach ( $banners as $i => $banner ) {
 
 			# Vars
-			$title = $banner['title'];
+			$title 	  = $banner['title'];
 			$subtitle = $banner['subtitle'];
-			$buttons = $banner['buttons'];
-			$bg_image = $banner['background_image'];
-			$bg_url = $bg_image['url'];
+			$buttons  = $banner['buttons'];
+			$bg__image = $banner['background_image'];
+			$bg__url   = $bg__image['url'];
 
 			# Lazyload all except first banner image
 			if ( $i == 0 ) {
-				$bg = 'style="background-image: url(' . $bg_url . ')"';
+				$bg = 'style="background-image: url(' . $bg__url . ')"';
 			} else {
-				$bg = 'data-bg="url(' . $bg_url . ')"';
+				$bg = 'data-bg="url(' . $bg__url . ')"';
 			}
 
 			# Begin Output
-			$html .= '<li class="banner__item" data-slidr="' . get_nums( $i ) . '" ' . $bg . '>';
-			$html .= '<div class="banner__content">';
+			if ($length > 1) {
+				$html .= '<li class="banner__item" data-slidr="' . get_nums( $i ) . '" ' . $bg . '>';
+			} else {
+				$html .= '<div class="banner__item">';
+			}
+
+			$html .= '<div class="block__container"><div class="banner__container">';
 
 			# Title
-			if ( $title ) {
-				$html .= '<h1 class="banner__title">' . $title . '</h1>';
-			}
+			if ( !!$title ) $html .= '<h1 class="banner__title">' . $title . '</h1>';
 
 			# Subtitle
-			if ( $subtitle ) {
-				$html .= '<h2 class="banner__subtitle">' . $subtitle . '</h2>';
-			}
+			if ( !!$subtitle ) $html .= '<h2 class="banner__subtitle">' . $subtitle . '</h2>';
 
 			# Buttons
-			if ( $buttons ) {
+			if ( !empty($buttons) ) {
 				$html .= '<div class="banner__buttons">';
 				foreach( $buttons as $button ) {
-					$button_text = $button['button_text'];
-					$button_link = $button['button_link'];
-					$is_modal = $button['is_modal'];
+					$button__text = $button['button_text'];
+					$button__link = $button['button_link'];
+					$is__modal = $button['is_modal'];
 
-					if ( $is_modal ) {
-						$html .= '<a href="' . $button_link . '" class="md-trigger">' . $button_text . '</a>';
+					if ( $is__modal ) {
+						$html .= '<a href="' . $button__link . '" class="md-trigger">' . $button__text . '</a>';
 					} else {
-						$html .= '<a href="' . $button_link . '">' . $button_text . '</a>';
+						$html .= '<a href="' . $button__link . '">' . $button__text . '</a>';
 					}
 				}
 				$html .= '</div>';
 			}
-			$html .= '</div>';
-			$html .= '</li>';
+			$html .= '</div></div>';
+
+			if ($length > 1) {
+				$html .= '</li>';
+			} else {
+				$html .= '</div>';
+			}
 		}
+
+		if($length > 1) $html .= '</ul>';
 		echo $html;
 	?>
 	</ul>
